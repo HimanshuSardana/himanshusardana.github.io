341e8378e0c7524d6e5b22109783195a5c92523a
Himanshu Sardana <55798183+HimanshuSardana@users.noreply.github.com>
2023-12-11 21:27:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ hamburger.addEventListener("click", mobileMenu);
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
 }
 
 function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
 }
 
 function addDarkmodeWidget() {
-  new Darkmode({ label: "🌙" }).showWidget();
+	new Darkmode({ label: "🌙" }).showWidget();
 }
 window.addEventListener("load", addDarkmodeWidget);
 
@@ -24,53 +24,53 @@ window.addEventListener("load", addDarkmodeWidget);
 var app = document.getElementById("description");
 
 var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 100,
+	loop: true,
+	delay: 100,
 });
 
 typewriter
-  .typeString("Software Engineer")
-  .pauseFor(1000)
-  .deleteChars(17)
-  .typeString("UI/UX Designer")
-  .pauseFor(1000)
-  .deleteChars(14)
-  .typeString("Web Developer")
-  .pauseFor(1000)
-  .start();
+	.typeString("Software Engineer")
+	.pauseFor(1000)
+	.deleteChars(17)
+	.typeString("UI/UX Designer")
+	.pauseFor(1000)
+	.deleteChars(14)
+	.typeString("Web Developer")
+	.pauseFor(1000)
+	.start();
 
 // IntersectionObserver
 const targets = document.querySelectorAll(".animate");
 
 const lazyLoad = (target) => {
-  const io = new IntersectionObserver(
-    (entries, observer) => {
-      console.log(entries);
+	const io = new IntersectionObserver(
+		(entries, observer) => {
+			console.log(entries);
 
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const img = entry.target;
-          img.classList.add("fade");
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					const img = entry.target;
+					img.classList.add("fade");
 
-          observer.disconnect();
-        }
-      });
-    },
-    {
-      threshold: 1,
-    }
-  );
-  io.observe(target);
+					observer.disconnect();
+				}
+			});
+		},
+		{
+			threshold: 1,
+		}
+	);
+	io.observe(target);
 };
 
 targets.forEach(lazyLoad);
 
 //preloader
 function preload() {
-  myVar = setTimeout(showPage, 3000);
+	myVar = setTimeout(showPage, 4300);
 }
 
 function showPage() {
-  document.querySelector(".spinner-wrapper").style.display = "none";
-  document.querySelector(".container").style.display = "block";
+	document.querySelector(".spinner-wrapper").style.display = "none";
+	document.querySelector(".container").style.opacity = 1;
 }
